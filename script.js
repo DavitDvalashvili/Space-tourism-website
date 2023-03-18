928ee5eb7d0fa8699cfe2d915bc4804c822b3621
davit dvalashvili <davitdvalashvili1996@gmail.com>
2023-03-18 20:01:39 +0400

    mobile version navigation and home page

---
+++ script.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector(".hamburger");
+const navbarIcon = document.querySelector("#navbar-icon");
+const navBar = document.querySelector(".navbar");
+const body = document.querySelector("body");
+
+
+//burger icon animation on click
+hamburger.addEventListener("click", () => {
+    //check if hamburger has active class.
+    let isActive = hamburger.classList.contains("active");
+    hamburger.classList.toggle("active");
+    navBar.classList.toggle("active");
+    if(!isActive) {
+        navbarIcon.src="./starter-code/assets/shared/icon-close.svg"
+    } else {
+        navbarIcon.src="./starter-code/assets/shared/icon-hamburger.svg"
+    }
+})
+
+
+body.addEventListener("resize", () => {
+    var width = body.width;
+    console.log(width);
+})
