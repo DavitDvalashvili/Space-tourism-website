b983bcfab7594d9989bde89ce5a2759657ba5184
davit dvalashvili <davitdvalashvili1996@gmail.com>
2023-03-24 23:45:54 +0400

    feat: finish markup and add some functions in js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navbarIcon = document.querySelector("#navbar-icon");
 const navBar = document.querySelector(".navbar");
-const body = document.querySelector("body");
-
 
 //burger icon animation on click
 hamburger.addEventListener("click", () => {
@@ -16,5 +14,34 @@ hamburger.addEventListener("click", () => {
         navbarIcon.src="./starter-code/assets/shared/icon-hamburger.svg"
     }
 })
+
+
+const getAdvice = async () => {
+    const response = await fetch('./data.json');
+    const data = await response.json();
+    //console.log(data);
+
+}
+getAdvice();
+
+
+const planetName = document.querySelector("#name");
+const planetDescription = document.querySelector(".description");
+const distance = document.querySelector(".distance .number");
+const takeTime = document.querySelector(".time-period .number");
+const destinationsNav = document.querySelectorAll(".planet-name");
+
+function displayMoon() {
+    destinationsNav.forEach(name => {
+        name.addEventListener("click", (event) => {
+            if (event.target.classList.contains("border-color")) {
+                console.log("true");
+            }
+        })
+    });
+
+}
+
+displayMoon();
 
 
